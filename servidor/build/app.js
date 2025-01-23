1de39581c3053491b32af891768f7fa02e797dbe
Camilo Castillo <camilo.castillo3090@gmail.com>
2025-01-23 13:09:55 -0500

    Build del proyecto

--- servidor/build/app.js
+++ servidor/build/app.js
@@ -1,1 +1,39 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = exports.browser = void 0;
+var _express = _interopRequireDefault(require("express"));
+var _morgan = _interopRequireDefault(require("morgan"));
+var _package = _interopRequireDefault(require("../package.json"));
+var _reservar = _interopRequireDefault(require("./routes/reservar.routes"));
+var _auth = _interopRequireDefault(require("./routes/auth.routes"));
+var _userpriv = _interopRequireDefault(require("./routes/userpriv.routes"));
+var _user = _interopRequireDefault(require("./routes/user.routes"));
+var _initialSetup = require("./libs/initialSetup");
+var _path = _interopRequireDefault(require("path"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var app = (0, _express["default"])();
+app.set('pkg', _package["default"]);
+(0, _initialSetup.crearRole)();
+app.set((0, _morgan["default"])('dev'));
+app.use(_express["default"].json());
+app.use(_express["default"].urlencoded({
+  extended: true
+}));
+app.use('/validation', _auth["default"]);
+app.use('/restr', _user["default"], _reservar["default"]);
+app.use('/priv', _userpriv["default"]);
+var basePath = _path["default"].resolve('..');
+app.use(_express["default"]["static"](_path["default"].join(basePath, 'cliente/build')));
+console.log(_path["default"].join(basePath, 'cliente/build', 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(_path["default"].join(basePath, 'cliente/build', 'index.html'));
+});
+var browser = exports.browser = function browser() {
+  if (typeof window !== "undefined") {
+    window.open('http://localhost:3000', '_blank');
+  }
+};
+var _default = exports["default"] = app;
--- servidor/build/routes/reservar.routes.js
+++ servidor/build/routes/reservar.routes.js
@@ -1,16 +1,19 @@
 "use strict";
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-var _app = _interopRequireWildcard(require("./app.js"));
-var db = _interopRequireWildcard(require("./database.js"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _express = require("express");
+var reservactrl = _interopRequireWildcard(require("../controllers/reserva.controller.js"));
+var _authjwt = require("../middlewares/authjwt.js");
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
-var port = 3000;
-try {
-  _app["default"].listen(port);
-  console.log("Se inicio el servidor en el puerto: ", port);
-  db.dbconnect();
-  (0, _app.browser)();
-} catch (error) {
-  console.log('Ha ocurrido un error al conectar', error);
-}
+var router = (0, _express.Router)();
+router.get('/reserva', [_authjwt.verifyToken, _authjwt.isAdminOrEmpleado], reservactrl.getReserva);
+router.get('/reserva/:reservaId', [_authjwt.verifyToken], reservactrl.getReservaId); // Operación por ID
+router.post('/reserva/new', [_authjwt.verifyToken, _authjwt.isUsuario], reservactrl.createReserva);
+router.put('/reserva/:reservaId', [_authjwt.verifyToken, _authjwt.isAdminOrEmpleado], reservactrl.updateReserva); // Operación por ID
+router["delete"]('/reserva/:reservaId', [_authjwt.verifyToken, _authjwt.isAdminOrEmpleado], reservactrl.deleteReserva); // Operación por ID
+var _default = exports["default"] = router;
