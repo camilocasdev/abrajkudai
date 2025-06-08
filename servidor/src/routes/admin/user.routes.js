11b7826e20fd5dbf94a5c828c8b627edad3305cc
Camilo Castillo <camilo.castillo3090@gmail.com>
2025-06-07 23:53:37 -0500

    Low level modification for Android App Testing

--- servidor/src/routes/admin/user.routes.js
+++ servidor/src/routes/admin/user.routes.js
@@ -1,14 +1,14 @@
 import {Router} from 'express';
 import * as usersctrl from '../../controllers/users.controller'
-import { isAdminOrEmpleado, verifyToken } from '../../middlewares/authjwt';
+import { isAdminOrEmpleado, refreshToken, verifyToken } from '../../middlewares/authjwt';
 
 const router = Router();
 
-router.get('/user/get/list',[verifyToken, isAdminOrEmpleado], usersctrl.get_user)
-router.get('/user/get/:userId',[verifyToken, isAdminOrEmpleado], usersctrl.get_user_by_id)
-router.post('/user/create',[verifyToken,isAdminOrEmpleado], usersctrl.add_user)
-router.put('/user/update/:userId',[verifyToken,isAdminOrEmpleado], usersctrl.update_user)
-router.delete('/user/delete/:userId',[verifyToken,isAdminOrEmpleado], usersctrl.del_user)
+router.get('/user/get/list',[ refreshToken ,verifyToken, isAdminOrEmpleado ], usersctrl.get_user)
+router.get('/user/get/:userId',[ refreshToken, verifyToken, isAdminOrEmpleado ], usersctrl.get_user_by_id)
+router.post('/user/create',[ refreshToken, verifyToken,isAdminOrEmpleado ], usersctrl.add_user)
+router.put('/user/update/:userId',[ refreshToken, verifyToken,isAdminOrEmpleado ], usersctrl.update_user)
+router.delete('/user/delete/:userId',[ refreshToken, verifyToken,isAdminOrEmpleado ], usersctrl.del_user)
 
 
 export default router;
